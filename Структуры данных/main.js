022da64717ed9761bb1d6c38086a3707d5f966fc
Egor4egg <97514500+Egor4egg@users.noreply.github.com>
2022-01-18 11:38:56 +0300

    Update main.js

--- Структуры данных/main.js
+++ Структуры данных/main.js
@@ -102,4 +102,32 @@ console.log(nth(arrayToList([10, 20, 30]), 1));
 */
 
 //глубокое сравнение ???
+function deepEqual(value1, value2){
+    if (value1 === value2){
+        return true;
+    }
+    if ((typeof value1 !="object" || value1 == null || typeof value2 !="object" || value2 == null)){
+        return false;
+    }
 
+    var properties1 = 0;
+    var properties2 = 0;
+    for (var prop in value1){
+        properties1++;
+    }
+    for (var prop in value2){
+        properties2++;
+        if (!(prop in value1 ) || !deepEqual(value1[prop],value2[prop])){
+            return false;
+        }
+    }
+    return properties1 == properties2;
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
