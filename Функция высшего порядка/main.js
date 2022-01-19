0ba68b974218c8014c847d53ea10538c75730d50
Egor4egg <egor.nabatov@actionengine.com>
2022-01-19 16:42:46 +0300

    Remarks

--- Функция высшего порядка/main.js
+++ Функция высшего порядка/main.js
@@ -4,19 +4,7 @@ console.log(array);
 // → [1, 2, 3, 4, 5, 6]
 
 
-function average(array) {
-    function plus(a, b) { return a + b; }
-    return array.reduce(plus) / array.length;
-  }
-  
-  var byName = {};
-  ancestry.forEach(function(person) {
-    byName[person.name] = person;
-  });
-  
-  
-  
-  // → 31.2
+
   
 
   
